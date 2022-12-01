02c2ffdacfac91d4c962c818da1e4af5716f49e7
andreassteffen <andreas.steffen@gmail.com>
2022-12-01 10:39:17 +0100

    beautify

--- 1/main.py
+++ 1/main.py
@@ -1,12 +1,16 @@
 import sys
+
 import pandas as pd
 
+
 def get_answers(file):
-    df = pd.read_table(file, header = None, skip_blank_lines = False, names = ['calories'])
+    df = pd.read_table(file, header=None, skip_blank_lines=False, names=['calories'])
     df.calories = df.calories.astype('Int64')
-    df['elve'] = df.calories.isna().cumsum() +1
-    df.dropna(inplace = True)
-    return {'max_cal': df.groupby('elve').sum().max().item(), 'top_three': df.groupby('elve').sum().sort_values('calories', ascending = False).head(3).sum().item()}
+    df['elve'] = df.calories.isna().cumsum() + 1
+    df.dropna(inplace=True)
+    return {'max_cal': df.groupby('elve').sum().max().item(),
+            'top_three': df.groupby('elve').sum().sort_values('calories', ascending=False).head(3).sum().item()}
+
 
 if __name__ == '__main__':
     fn = sys.argv[1]
